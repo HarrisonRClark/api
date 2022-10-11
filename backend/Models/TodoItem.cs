01aecd524973381e0ad23365acad0aafcefa6230
Harrison Clark <harrisonroyclark@gmail.com>
2022-10-11 21:17:32 +0100

    initial commit

---
+++ backend/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public long ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
