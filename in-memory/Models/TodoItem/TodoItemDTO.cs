d58bbacf2cd63363b038c1b63d0f493bcfecfaac
Harrison Clark <harrisonroyclark@gmail.com>
2022-10-12 10:26:40 +0100

    removed clunk

---
+++ in-memory/Models/TodoItem/TodoItemDTO.cs
@@ -0,0 +1,11 @@
+namespace TodoAPI.Models
+{
+    public class TodoItemDTO
+    {
+        public long ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
